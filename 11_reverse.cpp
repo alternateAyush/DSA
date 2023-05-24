0351c95d557bc63d9590b61491745fdf1f6c14d6
alternateAyush <ayushbhatt2002@gmail.com>
2023-05-24 22:03:22 +0530

    reverse SLL

---
+++ 11_reverse.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+Node *insert(Node *head, int a)
+{
+    Node *temp = new Node(a);
+    if (head == NULL)
+    {
+        return temp;
+    }
+    Node *curr = head;
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = temp;
+    return head;
+}
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+    return;
+}
+
+Node *reverse(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head = insert(head, 20);
+    head = insert(head, 30);
+    head = insert(head, 40);
+    head = insert(head, 50);
+    print(head);
+    return 0;
+}
