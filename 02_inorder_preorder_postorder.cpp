ca882dab79ba36593360dd85e1ad27948252b933
alternateAyush <ayushbhatt2002@gmail.com>
2023-06-11 22:37:09 +0530

    inorder preorder postorder traversal

---
+++ 02_inorder_preorder_postorder.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    Node *right;
+    Node *left;
+    int key;
+    Node(int k)
+    {
+        key = k;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+void inorder(Node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        cout << root->key << " ";
+        inorder(root->right);
+    }
+}
+
+void preorder(Node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->key << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(Node *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->key << " ";
+    }
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+    root->left->right->left = new Node(70);
+    root->left->right->right = new Node(80);
+    root->right->right = new Node(60);
+    //        10
+    //   20         30
+    // 40    50  NULL    60
+    //    70  80
+    cout << "Inorder: ";
+    inorder(root);
+    cout << endl;
+    cout << "Preorder: ";
+    preorder(root);
+    cout << endl;
+    cout << "Postorder: ";
+    postorder(root);
+    cout << endl;
+
+    return 0;
+}
